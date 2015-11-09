9e248d7b7497868f86ed26cd5c31405ca9ea9fe2
Forykx <prusa.vojtech@gmail.com>
2015-11-09 18:40:15 +0100

    SSHClient - not tested

---
+++ src/main/java/org/qe/hawkular/util/SSHClient.java
@@ -0,0 +1,89 @@
+package org.qe.hawkular.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
+import com.trilead.ssh2.Connection;
+import com.trilead.ssh2.Session;
+import com.trilead.ssh2.StreamGobbler;
+
+/**
+ * SSH client - SSHClient("user", "host", "password")
+ * @author vprusa
+ */
+public class SSHClient {
+    protected Connection connection = null;
+    protected Session session = null;
+    private final String user;
+    private final String host;
+    private final String pass;
+    protected static final Logger log = Logger.getLogger(SSHClient.class.getName());
+
+    public SSHClient(String user, String host, String pass) {
+        log.fine("Creating SSHClient that will connect to [" + user + "@" + host + "]");
+        this.user = user;
+        this.host = host;
+        this.pass = pass;
+    }
+
+    public static SSHClient fromProperties() {
+        String user, host, pass;
+        user = System.getProperty("notifSshUsername");
+        host = System.getProperty("notifSshHost");
+        pass = System.getProperty("notifSshPassword");
+        return new SSHClient(user, host, pass);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void execCommnad(String cmd) {
+        try {
+            session.execCommand(cmd);
+            InputStream stdout = new StreamGobbler(session.getStdout());
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+            String out = "";
+            while (true) {
+                String line = br.readLine();
+                if (line == null)
+                    break;
+                out += line + "\n";
+            }
+            out += "ExitCode: " + session.getExitStatus();
+            log.info(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        session.close();
+        connection.close();
+    }
+
+    public void connect() {
+        try {
+            connection = new Connection(host);
+            connection.connect();
+            boolean isAuthenticated = connection.authenticateWithPassword(user, pass);
+            if (isAuthenticated == false)
+                throw new IOException("Authentication failed.");
+            session = connection.openSession();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
