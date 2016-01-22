2d4c7c242918cb76604935de287773a44c2b582c
Valerii Sorokin <vsorokin@redhat.com>
2016-01-22 22:00:10 +0100

    refactored as required

--- src/test/java/org/qe/hawkular/tests/ExperimentalModeTest.java
+++ src/test/java/org/qe/hawkular/tests/ExperimentalModeTest.java
@@ -1,79 +1,46 @@
 package org.qe.hawkular.tests;
 
 import java.net.MalformedURLException;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.qe.hawkular.driver.HawkularSeleniumWebDriver;
 import org.qe.hawkular.driver.HawkularSeleniumLocalWebDriver;
 import org.qe.hawkular.element.HawkularInstallAgentConstants;
-import org.qe.hawkular.element.HawkularLoginPageConstants;
-import org.qe.hawkular.element.HawkularManagementConsolePageConstants;
 import org.qe.hawkular.element.HawkularRegistrationPageConstants;
 import org.qe.hawkular.page.HawkularLoginPage;
-import org.qe.hawkular.page.HawkularRegistrationPage;
-import org.qe.hawkular.util.HawkularDataProvider;
-import org.qe.hawkular.util.HawkularUtils;
-import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import com.saucelabs.testng.SauceOnDemandTestListener;
 
 public class ExperimentalModeTest extends HawkularSeleniumLocalWebDriver {
 	
 	WebDriver driver = null;	
 	
-	@BeforeSuite
-	public void prepareUser() throws MalformedURLException {
-		WebDriver driver = createLocalDriver();
-		HawkularRegistrationPage registration = new HawkularRegistrationPage(driver);
+	@BeforeMethod
+	public void startUp() throws MalformedURLException{
+		driver = createLocalDriver();
 		driver.get(HawkularSeleniumWebDriver.hawkularUrl);
-		System.out.println(driver.getTitle());
-		registration.registerUserIfDoesNotExist(
-				HawkularRegistrationPageConstants.username, 
-				HawkularRegistrationPageConstants.password, 
-				HawkularRegistrationPageConstants.confirmPassword, 
-				HawkularRegistrationPageConstants.firstName, 
-				HawkularRegistrationPageConstants.lastName, 
-				HawkularRegistrationPageConstants.email);
-	}	
-	
-	/*
-	@AfterMethod
-	public void closeSession() {
-		//driver.quit();
 	}
-	*/
-		
-	@Test
-	public void hawkularLoginTest()	throws Exception {
-		WebDriver driver = createLocalDriver();
-		driver.get(HawkularSeleniumWebDriver.hawkularUrl);
-
-		HawkularLoginPage loginPage = new HawkularLoginPage(driver);
-		loginPage.loginAs(HawkularRegistrationPageConstants.username, HawkularRegistrationPageConstants.password);		
-		
-		WebElement elem = driver.findElement(HawkularInstallAgentConstants.logoLocator);
-		elem.click();		
-
-		WebElement elemAI = driver.findElement(HawkularInstallAgentConstants.installAgentLocator);
-		askSubject(elemAI);
-		
-		elem.click();	
-		askSubject(elemAI);
-		
-		loginPage.logout();
+	
+	@AfterMethod
+	public void tearDown() {
 		driver.quit();
 	}
+	
+	@Test
+	public void hawkularLoginTest()	throws Exception {
+		HawkularLoginPage loginPage = new HawkularLoginPage(driver);
+		loginPage.loginAs(HawkularRegistrationPageConstants.username, HawkularRegistrationPageConstants.password);				
+		WebElement elem = driver.findElement(HawkularInstallAgentConstants.logoLocator);
+		elem.click();		
+		WebElement elemAI = driver.findElement(HawkularInstallAgentConstants.installAgentLocator);
+		askSubject(elemAI);		
+		elem.click();	
+		askSubject(elemAI);		
+	}
 
-	private void askSubject(Object asked) {
-		Reporter.log(HawkularInstallAgentConstants.testSubject + ((WebElement) asked).isDisplayed());
-		
+	private void askSubject(Object asked) {  	// -> utils ?
+		Reporter.log(HawkularInstallAgentConstants.testSubject + ((WebElement) asked).isDisplayed());		
 	}
 }
